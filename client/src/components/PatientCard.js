19a97209eff129f14d4b56153aa17c0d6aad99b0
WeronikaBL <weronika@datenexperten.com>
2023-08-05 17:56:03 +0200

    feat(client): mock patients array style: move cards to grid

--- client/src/components/PatientCard.js
+++ client/src/components/PatientCard.js
@@ -3,24 +3,27 @@ import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import { Grid } from '@mui/material';
 
-export default function PatientCard (props) {
+export default function PatientCard(props) {
   const { name = 'Patient', age = 100, id = 0 } = props;
   return (
-    <Card>
-      <CardContent>
-        <Typography variant="h5" component="div">
-          Name: {name}
-        </Typography>
-        <Typography sx={{ mb: 1.5 }} color="text.secondary">
-          Age: {age}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small" href={'patients/' + id}>
-          See patient details
-        </Button>
-      </CardActions>
-    </Card>
+    <Grid item xs={12} sm={6} md={4}>
+      <Card variant="outlined">
+        <CardContent>
+          <Typography variant="h5" component="div">
+            Name: {name}
+          </Typography>
+          <Typography sx={{ mb: 1.5 }} color="text.secondary">
+            Age: {age}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small" href={'patients/' + id} variant="outlined">
+            See patient details
+          </Button>
+        </CardActions>
+      </Card>
+    </Grid>
   );
 }
--- client/src/views/Patients/Patients.js
+++ client/src/views/Patients/Patients.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PatientCard from '../../components/PatientCard';
+import { Grid } from '@mui/material';
 
 const mockPatients = [
   { name: '333', age: 444444444, id: 1 },
@@ -9,9 +10,13 @@ function Patients () {
   return (
     <div className="container">
       <h2>Patients</h2>
+      <Grid container spacing={2}>
       {mockPatients.map(({ name, age, id }) => {
         return <PatientCard key={id} name={name} age={age} id={id} />;
       })}
+
+      </Grid>
+
     </div>
   );
 }
