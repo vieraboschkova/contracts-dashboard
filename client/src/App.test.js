171b2359c8e1c276b521d5ee3c9de47df89988e2
WeronikaBL <weronika@datenexperten.com>
2023-08-08 20:23:08 +0200

    test(client): add cards and price calculator test

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,24 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import Contract from './components/ContractCard';
+import PatientCard from './components/PatientCard';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('Contracts card to render contract Id', () => {
+  render(
+    <BrowserRouter>
+      <Contract contract={{ _id: '123' }} />
+    </BrowserRouter>,
+  );
+  const contract = screen.getByText(/123/i);
+  expect(contract).toBeInTheDocument();
+});
+
+test('Patients card to render name', () => {
+  render(
+    <BrowserRouter>
+      <PatientCard _id={'123'} name={'Test Doe'} birth={2000} />
+    </BrowserRouter>,
+  );
+  const patient = screen.getByText(/Test Doe/i);
+  expect(patient).toBeInTheDocument();
 });
